dc04a8e4b9ddd7a1f80cb2c9fd22a3562f7d2474
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-08-21 13:59:52 +0300

    Update quotes app and add an array of quotes using constructor method.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes:string[];
+
+
+  constructor(){
+    this.quotes = ['Watch finding nemo', 'Buy snacks', 'Go for a ride']
+  }
+  
 }
