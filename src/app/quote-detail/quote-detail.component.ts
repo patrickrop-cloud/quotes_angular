f7f1d44214ccb1775964b91f9f48033692aad2a4
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-08-22 12:16:59 +0300

    Add functionality of output binding to disply the quotes and description

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote'
 
 @Component({
@@ -9,6 +9,12 @@ import { Quote } from '../quote'
 export class QuoteDetailComponent implements OnInit {
 
   @Input() quote!: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+
+  quoteComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
 
   constructor() { }
 
