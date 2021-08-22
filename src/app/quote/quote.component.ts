7c69591ffb6ded82ec7d35d78c0731c54ee70bb0
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-08-22 23:38:06 +0300

    add an event emitter

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,25 +7,21 @@ import { Quote } from '../quote'
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  // quotes: any;
 
-  
-  quotes:Quote[] = [
+
+   quotes: Quote[] = [
     new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
-    new Quote(2,'Buy Snacks','Cookies and Candy are awsome'),
-    new Quote(3,'Go for a Ride','Take an evening ride through the park'),
-    new Quote(4,'Get Dog Food','Buy Bosko some food'),
-    new Quote(5,'Watch Angular Tutorial','Learn a new skill'),
-    new Quote(6,'Go to the Gym','Do some weight lifting'),
-    // {id:1, name:'Watch finding Nemo', description:'find an online version'},
-    // {id:2,name:'Buy snacks', description:'cookies and candy are awsome'},
-    // {id:3,name:'Go for a Ride', description:'Take an evening ride over the park'},
-    // {id:4,name:'Get Dog Food', description:'Buy Bosko some food'},
-    // {id:5,name:'Watch Angular tutorial', description:'learn a new skill'},
+    new Quote(2, 'Buy Snacks', 'Cookies and Candy are awsome'),
+    new Quote(3, 'Go for a Ride', 'Take an evening ride through the park'),
+    new Quote(4, 'Get Dog Food', 'Buy Bosko some food'),
+    new Quote(5, 'Watch Angular Tutorial', 'Learn a new skill'),
+    new Quote(6, 'Go to the Gym', 'Do some weight lifting'),
   ];
+  
   // toggleDetails(index){
   //   this.quotes[index].showDescription = !this.quotes[index].showDescription;
   // }
+  
 
   constructor() { }
 
