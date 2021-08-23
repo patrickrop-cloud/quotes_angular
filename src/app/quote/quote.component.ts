48d07994963ebe73c65d3030cad7b38b7101f075
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-08-23 03:07:47 +0300

    update the quotes.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,12 +10,12 @@ export class QuoteComponent implements OnInit {
 
 
    quotes: Quote[] = [
-    new Quote('Watch finding nemo.', 'Quote has 0 votes', 'Patrick', 'P.K',  0, 0, new Date(2016,7,10)),
-    new Quote('Go the Gym.', 'Quote has 0 votes', 'Peter', 'M.A',  0, 0, new Date(2020,1,12)),
-    new Quote('Watch Angular tutorial.', 'Quote has 0 votes', 'Raymond', 'R.E.D',  0, 0, new Date(1900,7,12)),
-    new Quote('Let the sleeping dog lie.', 'Quote has 0 votes', 'Jack.M', 'A.M',  0, 0, new Date(2009,7,12)),
-    new Quote('Life is what  you make it.', 'Quote has 0 votes', 'Jane. Doe', 'J.P.',  0, 0, new Date(2012,7,12)),
-    new Quote('Go for ride.', 'Quote has 0 votes', 'Kip. A', 'B.E',  0, 0, new Date(2019,7,12)),
+    new Quote('Live as if you were to die tomorrow. Learn as if you were to live forever', 'Quote has 0 votes', 'Mahatma Gandhi', 'M.G',  0, 0, new Date(2016,7,10)),
+    new Quote('It is true hard work never killed anybody, but I figure, why take the chance?.', 'Quote has 0 votes', 'Ronald Reagan', 'R.R',  0, 0, new Date(1987,22,4)),
+    new Quote('Two things are infinite: the universe and human stupidity; and I am not sure about the universe', 'Quote has 0 votes', 'Albert Einstein', 'A.E.',  0, 0, new Date(1960,5,11)),
+    new Quote('There are no bad pictures; that is just how your face looks sometimes..', 'Quote has 0 votes', 'Abraham Lincoln', 'A.L',  0, 0, new Date(2009,7,12)),
+    new Quote('Every man must decide whether he will walk in the light of creative altruism or in the darkness of destructive selfishness.', 'Quote has 0 votes', 'Martin Luther', 'M.L',  0, 0, new Date(1906,6,10)),
+    new Quote('Our deepest fear is not that we are inadequate. Our deepest fear is that we are powerful beyond measure.', 'Quote has 0 votes', 'Nelson Mandela', 'N.M',  0, 0, new Date(1920,7,12)),
     // new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
     // new Quote(2, 'Buy Snacks', 'Cookies and Candy are awsome'),
     // new Quote(3, 'Go for a Ride', 'Take an evening ride through the park'),
